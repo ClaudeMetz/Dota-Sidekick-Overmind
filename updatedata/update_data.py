11b451060ad51d6ebb2db790abf4ea0d512fb122
Claude Metz <claudearmetz@gmail.com>
2017-03-15 21:31:08 +0100

    Implement basic overseer class and add some more info when running update_data

--- updatedata/update_data.py
+++ updatedata/update_data.py
@@ -1,23 +1,28 @@
+from util.overseer import Overseer
 
 
 # Updates the production database for a new patch
 def update_data():
-    correct = "n"
-    while correct != "y":
-        patch = input("Enter the version number of the new patch: ")
-        correct = input("Is " + patch + " correct? (y/n): ")
-    print("Updating database...")
+    with Overseer() as overseer:
+        print(overseer.dev_mode())
+        print("Last recorded patch: " + overseer.patch_history)
 
-    print("Done!")
+        correct = "n"
+        while correct != "y":
+            patch = input("Enter the version number of the new patch: ")
+            correct = input("Is " + patch + " correct? (y/n): ")
+        print("Updating database...")
 
-    # Procedure step by step:
-    #   (Note: this procedure will be manually kicked off for now)
-    #   Info: Ask for patch name and other relevant data
-    # Setup: Setup (temporary) databases, folders through fitting handler classes/functions
-    # Crawling: Crawl items, heroes and their abilities and talents
-    # Manipulation: When necessary, manipulate the data further
-    # Saving: Save the polished data to the database through a handler
-    # Deployment: Deploy new data to production and clean up, save settings
+        print("Done!")
+
+        # Procedure step by step:
+        #   (Note: this procedure will be manually kicked off for now)
+        #   Info: Ask for patch name and other relevant data
+        # Setup: Setup (temporary) databases, folders through fitting handler classes/functions
+        # Crawling: Crawl items, heroes and their abilities and talents
+        # Manipulation: When necessary, manipulate the data further
+        # Saving: Save the polished data to the database through a handler
+        # Deployment: Deploy new data to production and clean up, save settings
 
 
 if __name__ == '__main__':
---
+++ util/overseer.py
@@ -0,0 +1,39 @@
+import json
+import os.path
+
+
+# Handles settings and general data for all parts of the project
+# Data: patch_history
+# Settings: dev_settings
+class Overseer:
+    # Loads all settings and data and saves it as instance variables
+    def __init__(self):
+        self.storage_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "storage"))
+
+        with open(os.path.join(self.storage_path, "data.json"), mode="r") as data_file:
+            data = json.loads(data_file.read())
+        self.patch_history = data["patch_history"]
+
+        with open(os.path.join(self.storage_path, "settings.json"), mode="r") as settings_file:
+            settings = json.loads(settings_file.read())
+        self.dev_settings = settings["dev_settings"]
+        self.languages = settings["languages"]
+
+    # For convenient use of 'with'
+    def __enter__(self):
+        return self
+
+    # Saves all data anew as it could have changed during runtime
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        data = {"patch_history": self.patch_history}
+        with open(os.path.join(self.storage_path, "data.json"), mode="w") as data_file:
+            data_file.write(json.dumps(data, indent=4))
+
+    # Checks if any dev-settings are enabled and returns a fitting status
+    def dev_mode(self):
+        message = "(dev-mode inactive)"
+        for setting in self.dev_settings.values():
+            if setting == 1:
+                message = "(dev-mode active)"
+                break
+        return message
