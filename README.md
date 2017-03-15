5ece62ddf26f88caece8165114d37cb1ec9128b9
Claude Metz <claudearmetz@gmail.com>
2017-03-15 20:09:03 +0100

    Formulate basic update_data structure and update README

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Dota-Sidekick-Backend-v2
-Second attempt at creating the paring backend for a Dota2-related app.
+Second attempt at creating the backend and API for a Dota2-related app. In the beginning, it focuses on providing an API for items, heroes and their abilities and talents (and corresponding images) that are up-to-date and versioned back to older patches.
---
+++ updatedata/update_data.py
@@ -0,0 +1,17 @@
+
+
+# Updates the production database for a new patch
+def update_data():
+    pass
+    # Procedure step by step:
+    # (Note: this procedure will be manually kicked off for now)
+    # Info: Ask for patch name and other relevant data
+    # Setup: Setup (temporary) databases, folders through fitting handler classes/functions
+    # Crawling: Crawl items, heroes and their abilities and talents
+    # Manipulation: When necessary, manipulate the data further
+    # Saving: Save the polished data to the database through a handler
+    # Deployment: Deploy new data to production and clean up, save settings
+
+
+if __name__ == '__main__':
+    update_data()
